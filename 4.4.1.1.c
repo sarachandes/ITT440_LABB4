c955870157fd2ef22391d10d24213253155d138e
sarachan <2020859818@isiswa.uitm.edu.my>
2021-05-08 18:36:45 +0000

    lab 4 server

---
+++ 4.4.1.1.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+#include <string.h>	//strlen
+#include <sys/socket.h>
+#include <arpa/inet.h>	//inet_addr
+#include <unistd.h>	//write
+
+int main(int argc , char *argv[])
+{
+	int socket_desc , new_socket , c;
+	struct sockaddr_in server , client;
+	char *message;
+	
+	//Create socket
+	socket_desc = socket(AF_INET , SOCK_STREAM , 0);
+	if (socket_desc == -1)
+	{
+		printf("Could not create socket");
+	}
+	
+	//Prepare the sockaddr_in structure
+	server.sin_family = AF_INET;
+	server.sin_addr.s_addr = INADDR_ANY;
+	server.sin_port = htons( 8888 );
+	
+	//Bind
+	if( bind(socket_desc,(struct sockaddr *)&server , sizeof(server)) < 0)
+	{
+		puts("bind gagal");
+		return 1;
+	}
+	puts("bind siap");
+	
+	//Listen
+	listen(socket_desc , 3);
+	
+	//Accept and incoming connection
+	puts("Menunggu sambungan...");
+	c = sizeof(struct sockaddr_in);
+	while( (new_socket = accept(socket_desc, (struct sockaddr *)&client, (socklen_t*)&c)) )
+	{
+		puts("Sambungan berjaya");
+		
+		//Reply to the client
+		message = "Hai Client! Glad we are connected, thank you and have a good day :D\n";
+		write(new_socket , message , strlen(message));
+	}
+	
+	if (new_socket<0)
+	{
+		perror("accept failed");
+		return 1;
+	}
+	
+	return 0;
+}
